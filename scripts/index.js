e366ab7d6bb3b05f7f474ffe617f36becd8a0bd0
Adam Atrash <104771785+Blackthorn11@users.noreply.github.com>
2022-07-17 11:41:55 +0300

    project 4

--- scripts/index.js
+++ scripts/index.js
@@ -39,3 +39,37 @@ function closeModal() {
 
 editButton.addEventListener("click", openModal);
 closeButton.addEventListener("click", closeModal);
+
+const profileFormElement = document.querySelector(".modal__form");
+
+const nameInput = document.querySelector("#nameInput");
+const jobInput = document.querySelector("#jobInput");
+
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closeModal();
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+const cardTemplate =
+  document.querySelector("#cardTemplate").content.firstElementChild;
+
+const cardList = document.querySelector(".cards__list");
+
+initialCards.forEach(function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  cardList.appendChild(cardElement);
+});
